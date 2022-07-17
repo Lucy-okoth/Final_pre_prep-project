e1ef9bf9d6fb5425de0960d0fc6a0c9e45620c64
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-07-17 20:45:49 +0300

    modify main.js

--- assets/js/js main.js
+++ assets/js/js main.js
@@ -2,7 +2,7 @@ const sheds=["A","B","C","D"]
 const prices=[510, 308, 486, 572]
 let total=0;
 for(let index=0;index<prices.length;index++){
-    console.log("produce per shed is"+sheds[index]+"is"+prices[index]+"per day")
+    console.log(" your produce in shed is"+sheds[index]+"is"+prices[index]+"litres per day")
     total=total+prices[index]
 }
 console.log("the total production is"+total+"litres per day")
