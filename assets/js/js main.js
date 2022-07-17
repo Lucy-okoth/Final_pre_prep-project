5724387f4c1a86db138b6bb2099f0b9f21299cbc
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-07-17 21:17:00 +0300

    add comment in  main.js

--- assets/js/js main.js
+++ assets/js/js main.js
@@ -1,8 +1,10 @@
+/*Calculation for daily produce/litre*/
 const sheds=["A","B","C","D"]
 const prices=[510, 308, 486, 572]
 let total=0;
 for(let index=0;index<prices.length;index++){
-    console.log(" your produce in shed is"+sheds[index]+"is"+prices[index]+"litres per day")
+    console.log(" your produce in shed"+sheds[index]+"is"+prices[index]+"litres per day")
     total=total+prices[index]
 }
+
 console.log("the total production is"+total+"litres per day")
