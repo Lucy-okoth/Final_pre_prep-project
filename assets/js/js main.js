77c0a00d69f790f0bdc0c4b1e57016649fe325a3
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-07-17 20:37:03 +0300

    add values in main.js

--- assets/js/js main.js
+++ assets/js/js main.js
@@ -1,1 +1,8 @@
-alert("Js is working")
+const sheds=["A","B","C","D"]
+const prices=[510,308,486,572]
+let total=0;
+for(let index=0;index<prices.length;index++){
+    console.log("produce per shed is"+sheds[index]+"is"+prices[index]+"per day")
+    total=total+prices[index]
+}
+console.log("the total production is"+total+"litres per day")
