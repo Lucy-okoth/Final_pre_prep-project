00e998f2d582b2a1666b880019f4701a5ff2f78f
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-07-17 20:38:20 +0300

    add spacing between numbers

--- assets/js/js main.js
+++ assets/js/js main.js
@@ -1,5 +1,5 @@
 const sheds=["A","B","C","D"]
-const prices=[510,308,486,572]
+const prices=[510, 308, 486, 572]
 let total=0;
 for(let index=0;index<prices.length;index++){
     console.log("produce per shed is"+sheds[index]+"is"+prices[index]+"per day")
